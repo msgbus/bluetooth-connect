06c72b76ba716d89e606f2434b87b03aa1524da5
Zhang Hu <iamzhanghu@gmail.com>
2019-11-03 23:11:07 +0800

    device list

--- App/Views/Settings/index.js
+++ App/Views/Settings/index.js
@@ -4,17 +4,26 @@ import t from '@Localize'
 import config from '@Config'
 import styles from '@Styles'
 import Icon from '@Components/Icon'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Picker } from 'react-native'
 import { ListItem } from 'react-native-elements'
 import AwesomeIcon from 'react-native-vector-icons/FontAwesome'
 import req from '@Network'
 import BluetoothReq from '../../Bluetooth';
+import * as RNLocalize from "react-native-localize";
+import {getCurrentLanguage} from "../../Localize";
+import storage from '@Utils/storage'
 
 @connect(state => ({
   user: state.app.user
 }))
 
 export default class HomeScreen extends React.Component {
+  state = {currentDevice: ''}
+  updateCurrentDevice = (deviceUUID) => {
+    console.log('current device', deviceUUID)
+    this.setState({ currentDevice: deviceUUID })
+  }
+
   static navigationOptions = _ => {
     return {
       ...config.defaultNavigation,
@@ -33,14 +42,14 @@ export default class HomeScreen extends React.Component {
           this.props.navigation.navigate('Language')
         }
       },
-      {
-        title: t('settings.feedback'),
-        icon: 'feedback2',
-        color: '#0c9',
-        onPress() {
-          this.props.navigation.navigate('Feedback')
-        }
-      },
+      // {
+      //   title: t('settings.feedback'),
+      //   icon: 'feedback2',
+      //   color: '#0c9',
+      //   onPress() {
+      //     this.props.navigation.navigate('Feedback')
+      //   }
+      // },
       {
         title: t('settings.about'),
         icon: 'about1',
@@ -48,38 +57,30 @@ export default class HomeScreen extends React.Component {
         onPress() {
           this.props.navigation.navigate('About')
         }
-      },
-      {
-        title: t('settings.bluetooth'),
-        icon: 'ios7chatbubbleoutline',
-        color: '#09f',
-        onPress() {
-          this.props.navigation.navigate('Bluetooth')
-        }
       }
     ]
   }
 
   render() {
     return (
-      <View style={viewStyles.container}>
+    <View style={viewStyles.container}>
+      <Picker selectedValue={this.state.currentDevice} onValueChange={this.updateCurrentDevice}>
+        {
+          this.getBoundDevices().map((item, i) => (
+            <Picker.Item label={item.name} value={item.uuid}/>
+          ))
+        }
+      </Picker>
+      <View style={viewStyles.list}>
         <ListItem
           chevron
           topDivider
           bottomDivider
-          leftAvatar={{
-            size: 65,
-            source: {
-              uri: this.props.user.avatar_url
-            }
-          }}
-          title={this.props.user.nick_name}
-          titleStyle={{ fontSize: 23 }}
-          subtitle={t('settings.location') + ': ' + this.props.user.location}
-          subtitleStyle={{ fontSize: 16, color: '#858585' }}
-          onPress={_ => { this.props.navigation.navigate('Profile') }}
-        />
-        {
+          leftIcon={<AwesomeIcon name='bluetooth' style={{fontSize: 26, color: '#fc3'}}/>}
+          onPress={_ => { this.props.navigation.navigate('Bluetooth') }}
+          title={t('settings.addDevice')}/>
+      </View>
+      {
           this.menuList.map((item, i) => (
             <View style={viewStyles.list} key={i}>
               <ListItem
@@ -94,18 +95,45 @@ export default class HomeScreen extends React.Component {
             </View>
           ))
         }
-        <View style={viewStyles.list}>
-          <ListItem
-            chevron
-            topDivider
-            bottomDivider
-            leftIcon={<AwesomeIcon name='bluetooth' style={{fontSize: 26, color: '#fc3'}}/>}
-            onPress={_ => { this.props.navigation.navigate('Bluetooth') }}
-            title={t('settings.addDevice')}/>
-        </View>
       </View>
     )
   }
+
+  deviceItems() {
+    const devices = this.getBoundDevices();
+    return devices.map((item, i) => (
+      <View style={viewStyles.list} key={i}>
+        <ListItem
+          chevron
+          topDivider
+          bottomDivider
+          title={item.name}
+          onPress={_ => { this.props.navigation.navigate('About') }}
+          leftIcon={<AwesomeIcon name='bluetooth' style={{fontSize: 26, color: '#fc3'}}/>}
+        />
+      </View>
+    ))
+  }
+
+  getBoundDevices() {
+    const devices = storage.get('boundDevices')
+    // const devices = [
+    //   {
+    //     name: 'Weport T1',
+    //     type: 'Weport T1',
+    //     uuid: 'uuid1'
+    //   },
+    //   {
+    //     name: 'Weport T2',
+    //     type: 'Weport T2',
+    //     uuid: 'uuid2'
+    //   }
+    // ]
+    console.log('getBoundDevices', devices)
+
+    return devices instanceof Array ? devices : [];
+  }
+
 }
 
 const viewStyles = StyleSheet.create({
